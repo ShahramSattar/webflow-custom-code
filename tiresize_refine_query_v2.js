5c24e77c5efa3e950a33db618f2753ec64cf4986
Shahram Sattar <shahram.sattar@ryerson.ca>
2020-10-15 01:00:09 -0400

    Update and rename tiresize_refine_query_v1.js to tiresize_refine_query_v2.js

--- tiresize_refine_query_v2.js
+++ tiresize_refine_query_v2.js
@@ -3,17 +3,6 @@ $(document).ready(function(){
     load_json_data()
     load_json_data_wrd()
     function load_json_data() {
-        let html_code = '';
-        //let nameCapitalized = id.charAt(0).toUpperCase() + id.slice(1)
-        html_code += '<option value="">Select Year</option>';
-        //html_code += '<option value="">Select Make</option>';
-        // html_code += '<option value="">Select Model</option>';
-        // html_code += '<option value="">Select Tire Option</option>';
-        $.getJSON(url+ '/queryall_ymmt', function (data) {
-            $.each(data, function (key, value) {
-                html_code += '<option value="' + key + '">' + value["Year"] + '</option>';
-            });
-            $('#year').html(html_code);
             const queryString = window.location.search;
             const urlParams = new URLSearchParams(queryString);
             const year = urlParams.get('year')
@@ -21,11 +10,26 @@ $(document).ready(function(){
             const model = urlParams.get('model')
             const trim = urlParams.get('trim')
             const tiresize = urlParams.get('tiresize')
-            $('#make').html('<option value="">' + make + '</option>');
-            $('#model').html('<option value="">' + model + '</option>');
-            $('#trim').html('<option value="">'+ trim + '</option>');
-            $('#tiresize').html('<option value="">' + tiresize + '</option>');
-        });
+            let html_code = '';
+            //let nameCapitalized = id.charAt(0).toUpperCase() + id.slice(1)
+            html_code += '<option value="">Select Year</option>';
+            //html_code += '<option value="">Select Make</option>';
+            // html_code += '<option value="">Select Model</option>';
+            // html_code += '<option value="">Select Tire Option</option>';
+            $.getJSON(url+ '/queryall_ymmt', function (data) {
+                $.each(data, function (key, value) {
+                    if (value["Year"] == year.toString()){
+                        html_code += '<option value="' + key + '"selected="selected' +'">' + value["Year"] + '</option>';
+                    }else{
+                        html_code += '<option value=' + key + '>' + value["Year"] + '</option>';
+                    }
+                });
+                $('#year').html(html_code);
+                query_select_ymmt('make',make, year);
+                query_select_ymmt('model',model, year,make);
+                query_select_ymmt('trim',trim, year,make,model);
+                query_select_ymmt('tiresize',tiresize, year,make,model,trim);
+            });
     }
     function load_json_data_wrd() {
         let html_code = '';
@@ -74,6 +78,40 @@ $(document).ready(function(){
             }
         });
     }
+            function query_select_ymmt(id,svalue, year_name, make_name, model_name, trim_name, selected_size) {
+            let html_code = '';
+            let link =''
+            let nameCapitalized = ''
+            if(id == "tiresize"){
+                nameCapitalized = "Tire Option"
+            }else{
+                nameCapitalized = id.charAt(0).toUpperCase() + id.slice(1)
+            }
+            html_code += '<option value="">Select '+nameCapitalized+'</option>';
+            if (id == "make") {
+                link = url + '/query_ymmt/make/?' + 'year=' + year_name
+            }else if (id == "model") {
+                link = url + '/query_ymmt/model/?' + 'year=' + year_name + '&make=' + make_name
+            }else if (id == "trim") {
+                link = url + '/query_ymmt/trim/?' + 'year=' + year_name + '&make=' + make_name + '&model=' + model_name
+            }else if (id == "tiresize") {
+                link = url + '/query_ymmt/tiresize/?' + 'year=' + year_name + '&make=' + make_name + '&model=' + model_name + '&trim=' + trim_name
+            }else if (id == "matched") {
+                link = url + '/query_ymmt/tiresize/?' + 'year=' + year_name + '&make=' + make_name + '&model=' + model_name + '&trim=' + trim_name + '&TireSize=' + selected_size
+            }
+            $.getJSON(link, function (data, status) {
+                if (status == "success") {
+                    $.each(data, function (key, value) {
+                        if (value[id] == svalue.toString()){
+                            html_code += '<option value="' + key + '"selected="selected' +'">' + value[id] + '</option>';
+                        }else{
+                            html_code += '<option value=' + key + '>' + value[id] + '</option>';
+                        }
+                    });
+                    $('#' + id).html(html_code);
+                }
+            });
+        }
     function load_json_data_wrd_find(id, year_name, make_name, model_name, trim_name, selected_size) {
         let html_code = '';
         let link =''
